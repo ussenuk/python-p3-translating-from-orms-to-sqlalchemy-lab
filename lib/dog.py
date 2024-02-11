78bb053758047a45c3ea8740f46505e7092d8357
ussenuk <ukimanuka@gmail.com>
2024-02-11 19:16:54 +0300

    transfering the knowledge frpm orm to sqlalchemy

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,78 @@
-from models import Dog
+from models import Dog, Base
+from sqlalchemy import (create_engine, desc, func,Column, String, Integer)
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+
+
+def create_table(Base, engine):
+    # engine = create_engine('sqlite:///dogs.db')
+    return Base.metadata.create_all(engine)
+    
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name.like(name)).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    # dog = session.query(Dog).filter_by(Dog.name == {dog}).first()
+    # dog = session.query(Dog).filter(Dog.name == dog).first()
+    if dog: 
+        dog.breed = breed
+        session.commit()
+    else:
+        print(f"No dog found with the name {dog}")
+
+    
+
+# if __name__ == '__main__':
+#     from sqlalchemy import (create_engine, desc, func,Column, String, Integer)
+#     from sqlalchemy.orm import sessionmaker
+
+#     engine = create_engine('sqlite:///dogs.db')
+#     # Base.metadata.create_all(engine)
+
+#     # use our engine to configure a 'Session' class
+#     Session = sessionmaker(bind=engine)
+#     # use 'Session' class to create 'session' object
+#     session = Session()
+
+#     # create_table(Base, engine)
+
+#     # dog = Dog(name="Newton", breed="Corgi")
+
+#     # save(session, dog)
+
+#     names = [name for name in session.query(Dog.name, Dog.breed)]
+
+#     print(names)
+
+#     get_all(session)
+
+#     find_by_name(session, "Albert")
+
+#     # for record in query: print(record.name, record.breed)
+
+#     find_by_id(session, id=3)
+#     # for record in query2: print(record.name, record.breed)
+
+    
+#     find_by_name_and_breed(session, name="Newton", breed="Bulldog")
+#     # for record in query3: print(record.name, record.breed)
+
+
+#     # dog = session.query(Dog).filter_by(name='Newton').first()
+#     # update_breed(session, dog, breed="chihuahua")
+
+#     # for record in query: print(record.name and record.breed)
--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (create_engine, desc, func,Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -11,3 +12,34 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+# if __name__ == '__main__':
+    
+#     engine = create_engine('sqlite:///:memory:')
+#     Base.metadata.create_all(engine)
+
+#     # use our engine to configure a 'Session' class
+#     Session = sessionmaker(bind=engine)
+#     # use 'Session' class to create 'session' object
+#     session = Session()
+
+
+#     einstein = Dog(
+#             name="Einstein",
+#             breed="Chiwawa",
+#         )
+
+#     turing = Dog(
+#         name="Turing",
+#         breed="German Sherperd",
+#     )
+
+#     session.bulk_save_objects([einstein, turing])
+#     session.commit()
+
+#     # read records
+#     # dogs = session.query(Dog).all()
+
+#     names = [name for name in session.query(Dog.name, Dog.breed)]
+
+#     print(names)
